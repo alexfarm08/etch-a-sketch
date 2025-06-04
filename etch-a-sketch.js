a297735627ecfc7f34b2c11fc83f5afbc6b68a98
alexfarm08 <209876587+alexfarm08@users.noreply.github.com>
2025-06-03 18:21:45 -0700

    added the rest of the code to finish etch-a-sketch project jun 3, 2025

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,20 +1,46 @@
 // set up grid 
 let dimensions = 16;
 
+// function makes grid
 function makeGrid(dimensions) {
     const container = document.querySelector(`.container`);
 
+    // loops through dimensions and makes new div to make grid
     for (let i = 0; i < dimensions; i++) {
         const parentDiv = document.createElement('div.parent');
         parentDiv.classList.add('parentDiv');
         for (let j = 0; j < dimensions; j++) {
             let div = document.createElement('div.col');
             div.classList.add('col');
+            // sets up event listener for hover
+            div.addEventListener("mouseover", event => {
+                event.target.style.backgroundColor = 'black';
+            })
             parentDiv.appendChild(div);
         }
         container.appendChild(parentDiv);
     }
+
 }
-//const div = document.createElement('div.row');
-//div.classList.add('row');
-console.log(makeGrid(dimensions));
+
+// calls the make grid function to make grid of specified size
+makeGrid(dimensions);
+
+const gridBtn = document.querySelector('#sizeBtn');
+
+gridBtn.addEventListener('click', event => {
+    var removeCanvas = document.querySelectorAll('.parentDiv');
+
+    // asks user for to input num 0 - 100
+    do {
+        dimensions = prompt("Enter a number for the hight and width of your canvas over 0 and under 100");
+    } while(dimensions < 0 || dimensions > 100);
+
+    // removes old grid
+    for (let i = 0; i < removeCanvas.length; i++) {
+        removeCanvas[i].remove();
+    }
+
+    // makes new grid
+    makeGrid(dimensions);
+});
